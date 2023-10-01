9fdaf240bf7c508c8532efed31e163c08c2edddb
Ryan <jose.ryan0689@gmail.com>
2023-10-01 09:59:36 -0300

    transformando projeto em react

---
+++ Projeto_Integrador_Official/carshopping/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './assets/img/logotipo.png';
+import background from './assets/img/background.jpg';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App;
---
+++ Projeto_Integrador_Official/carshopping/src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import './Footer.css';
+
+function Footer(){
+    return(
+        <footer>
+            <nav className="rodape">
+                <ul>
+                    <li><a href="../html/Sobre.html">Sobre</a></li>
+                    <li><a href="../html/Servicos.html">Serviços</a></li>
+                    <li><a href="../html/Contatos.html">Contato</a></li>
+                    <li><a href="../html/Suporte.html">Suporte</a></li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer
